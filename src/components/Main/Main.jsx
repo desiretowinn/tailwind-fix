6e8c12f59fc8090a02e619563782be854ed357f2
Diyorbek Abduganiyev <diyorbekabduganiyev@MacBook-Air-Diyorbek.local>
2023-04-10 13:05:10 +0500

    saperate cards data

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,65 +1,16 @@
 import React from 'react';
 import "./index.scss"
 import Card from '../././../UI/Card/Card'
+import FuturedCard from '../../UI/Card/FuturedCard';
+import PostCard from '../../UI/Card/PostCard';
+import { heroCard } from '../../constants/CardsInfo';
+import { postCard } from '../../constants/CardsInfo';
+import { futuredCard } from '../../constants/CardsInfo';
 
 import img1 from "../../assets/images/avatar.png"
 import img2 from "../../assets/images/card-imh.png"
-import PostCard from '../../UI/Card/PostCard';
-import FuturedCard from '../../UI/Card/FuturedCard';
 
 const Main = () => {
-
-
-    function card(props) {}
-
-    const heroCard = [
-        {
-            "title": "Hi, I am John, Creative Technologist",
-            "description": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
-            "btn": "Download Resume",
-            "img": img1,
-        }
-    ]
-
-    function post(props){}
-
-    const postCard = [
-        {
-            "title": "Making a design system from scratch",
-            "data": "12 Feb 2020     |      Design, Pattern",
-            "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-        },
-        {
-            "title": "Creating pixel perfect icons in Figma",
-            "data": "12 Feb 2020     |      Figma, Icon Design",
-            "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-        }
-    ]
-
-    function futured(props){}
-
-    const futuredCard = [
-        {
-            "title": "Designing Dashboards",
-            "year": "2020",
-            "img": img2,
-            "data": "Dashboard",
-            "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-        },
-        {
-            "title": "Vibrant Portraits of 2020",
-            "year": "2018",
-            "img": img2,
-            "data": "Illustration",
-            "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-        },        {
-            "title": "36 Days of Malayalam type",
-            "year": "2018",
-            "img": img2,
-            "data": "Typography",
-            "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-        },
-    ]
 
     return (
         <>
---
+++ src/constants/CardsInfo.jsx
@@ -0,0 +1,54 @@
+import img1 from "./.././assets/images/avatar.png";
+import img2 from "./.././assets/images/card-imh.png"
+
+// -------- Hero Cards --------
+
+export const heroCard = [
+    {
+        "title": "Hi, I am John, Creative Technologist",
+        "description": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.",
+        "btn": "Download Resume",
+        "img": img1,
+    }
+]
+
+// -------- Posts Cards --------
+
+export const postCard = [
+    {
+        "title": "Making a design system from scratch",
+        "data": "12 Feb 2020     |      Design, Pattern",
+        "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+    {
+        "title": "Creating pixel perfect icons in Figma",
+        "data": "12 Feb 2020     |      Figma, Icon Design",
+        "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    }
+]
+
+// -------- Futured Cards --------
+
+
+export const futuredCard = [
+    {
+        "title": "Designing Dashboards",
+        "year": "2020",
+        "img": img2,
+        "data": "Dashboard",
+        "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+    {
+        "title": "Vibrant Portraits of 2020",
+        "year": "2018",
+        "img": img2,
+        "data": "Illustration",
+        "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },        {
+        "title": "36 Days of Malayalam type",
+        "year": "2018",
+        "img": img2,
+        "data": "Typography",
+        "text": "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+    },
+]
